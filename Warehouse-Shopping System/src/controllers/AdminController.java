c6ab4347afc02d6cf4b3f245ed0dde8df74f6493
Aashna Vasudeva <aashna.vasudeva@mail.utoronto.ca>
2020-12-27 18:23:56 -0500

    Created admin functionality

---
+++ Warehouse-Shopping System/src/controllers/AdminController.java
@@ -0,0 +1,63 @@
+package controllers;
+
+import entities.Admin;
+import entities.Buyer;
+import entities.Complaint;
+import usecases.AdminAccount;
+
+import java.util.ArrayList;
+
+public class AdminController {
+    private String username;
+    private AdminAccount adminAccount;
+
+    public AdminController(String username, AdminAccount aA){
+        this.username = username;
+        adminAccount = aA;
+    }
+
+    public void createAdminAccount(String username, String password){
+        if (password.length() >= 8 && !adminAccount.getAllUsers().containsKey(username)) {
+            adminAccount.addUser("admin", username, password);
+        }
+    }
+
+    public void callMessageUser(String userID, String message){
+        if (adminAccount.getAllUsers().containsKey(userID) && !message.equals("")){
+            adminAccount.messageUser(userID, message);
+        }
+    }
+
+    public String viewComplaints(){
+        StringBuilder output = new StringBuilder();
+        Admin admin = adminAccount.getAdmin().get(username);
+        for (String compID: admin.getComplaints()){
+            Complaint curr = adminAccount.getComplaints().get(compID);
+            output.append("Complaint: ").append(curr.getComplaintContent()).append(" ");
+        }
+        return output.toString();
+    }
+
+    public void callChangeAttribute(String itemID, String attribute, String change){
+        ArrayList<String> allAttributes = new ArrayList<>();
+        allAttributes.add("material");
+        allAttributes.add("stock");
+        allAttributes.add("colour");
+        allAttributes.add("item name");
+        if (adminAccount.getAllItems().containsKey(itemID) && allAttributes.contains(attribute) && !change.equals("")){
+            adminAccount.changeAttribute(itemID, attribute, change);
+        }
+    }
+
+    public void callDeleteUser(String username){
+        if (adminAccount.getAllUsers().containsKey(username)){
+            adminAccount.deleteUser(username);
+        }
+    }
+
+    public void changePassword(String oldPass, String newPass){
+        if (!oldPass.equals(newPass) && newPass.length() >= 8){
+            adminAccount.changePassword(this.username, newPass);
+        }
+    }
+}
---
+++ Warehouse-Shopping System/src/usecases/AdminAccount.java
@@ -0,0 +1,48 @@
+package usecases;
+import entities.*;
+
+public class AdminAccount extends UserAccount {
+
+    public AdminAccount(){}
+
+    public void messageUser(String userID, String message){
+        if (buyers.containsKey(userID)){
+            buyers.get(userID).getMessageReceived().add(message);
+        }
+        else if (seller.containsKey(userID)){
+            seller.get(userID).getMessageReceived().add(message);
+        }
+    }
+
+    public void changeAttribute(String itemID, String attributeType, String change){
+        if (attributeType.equalsIgnoreCase("material")){
+            allItems.get(itemID).setMaterial(change);
+        }
+        else if (attributeType.equalsIgnoreCase("colour")){
+            allItems.get(itemID).setColour(change);
+        }
+        else if (attributeType.equalsIgnoreCase("item name")){
+            allItems.get(itemID).setItemName(change);
+        }
+        else if (attributeType.equalsIgnoreCase("stock")){
+            allItems.get(itemID).setStock(Integer.parseInt(change));
+        }
+    }
+
+    public void deleteUser(String username){
+        allUsers.remove(username);
+        buyers.remove(username);
+        if (seller.containsKey(username)){
+            for (Item item : allItems.values()){
+                if (item.getSellerID().equals(username)){
+                    allItems.remove(item.getItemID());
+                }
+            }
+            seller.remove(username);
+        }
+    }
+
+
+
+
+}
